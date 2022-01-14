5e3cb1e219e7013f47ba21d9af5b275bcbb55b98
Dallas Cohrs <dallas.cohrs.dev@gmail.com>
2022-01-14 12:12:12 -0700

    changed text to bruh 2

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>bruh!</Text>
+      <Text>bruh 2!</Text>
       <StatusBar style="auto" />
     </View>
   );
